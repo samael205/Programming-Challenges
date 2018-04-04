25a529c54c9b0abae1ee79196d45047d80898200
blooser <Blooser@protonmail.com>
2018-04-05 01:18:01 +0200

    add count character option

--- WordsCounter/main.cpp
+++ WordsCounter/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <future>
 #include <numeric>
+#include <cstring>
 
 #include "wordsCounter.h"
 
 #define REP(i, n, v) for(int i=v; i<n; i++)
 
+void findOptions(int argc, char ** argv);
+
 int main(int argc, char *argv[]){
 	if(argc < 2)
 		std::exit(EXIT_FAILURE);
 
+	findOptions(argc, argv);
+
 	wc::filesContent content;
 
 	REP(i, argc, 1){
+		if(std::strlen(argv[i]) == 0)
+			continue;
+
 		std::ifstream file;
 		file.open(argv[i]);
 
@@ -34,6 +42,23 @@ int main(int argc, char *argv[]){
 	for(auto it = threads.begin(); it != threads.end(); it++)
 		it->get();		
 
-	int allWords = std::accumulate(wc::words.begin(), wc::words.end(), 0);
-	std::cout<<"Words:\t"<<allWords<<'\n';
+
+	auto count = std::accumulate(wc::words.begin(), wc::words.end(), std::make_pair<int, int>(0, 0),
+		wc::sumPair<int, int>());
+	
+	std::cout<<"Words:\t"<<count.first;	
+	if(wc::readChars)
+		std::cout<<"\tChars:\t"<<count.second;
+	std::cout<<"\n";
 }
+
+void findOptions(int argc, char ** arguments){
+	REP(i, argc, 1)
+		if(std::strcmp(arguments[i], "--chars") == 0 ||
+			std::strcmp(arguments[i], "-c") == 0){
+			wc::readChars = true;
+			arguments[i][0] = '\0';
+			break;
+		}
+}
+
--- WordsCounter/wordsCounter.h
+++ WordsCounter/wordsCounter.h
@@ -4,14 +4,19 @@
 #include <sstream>
 #include <iterator>
 #include <mutex>
+#include <utility>
+#include <algorithm>
+#include <cctype>
 
 namespace wc{
 	typedef std::vector<std::string> filesContent;
-	typedef std::vector<int> counter;
+	typedef std::vector<std::pair<int, int>>counter;
 
 	static counter words;
 
 	std::mutex threadGuard;
+
+	bool readChars = false;
 
 	std::string getFileContent(std::ifstream & file){
 		std::string bufor = "\0";
@@ -23,14 +28,24 @@ namespace wc{
 		return data;
 	}
 
+	template <class T1, class T2>
+	struct sumPair : public std::binary_function<std::pair<T1, T2>, std::pair<T1, T2>, std::pair<T1, T2>>{
+		std::pair<T1, T2> operator()(const std::pair<T1, T2>&a, const std::pair<T1, T2>&b){
+			return std::pair<T1, T2>(a.first + b.first, a.second + b.second);
+		}
+	};
+
 	void countWords(const std::string & content){
 		std::stringstream sstream(content);
 		int numberOfWords = std::distance(std::istream_iterator<std::string>(sstream), 
 			std::istream_iterator<std::string>());
 
+		int numberOfChars = std::count_if(content.begin(), content.end(), [](char c){
+			return std::isalpha(c);
+		});
+
 		threadGuard.lock();
-		words.push_back(numberOfWords);
+		words.push_back(std::pair<int, int>(numberOfWords, numberOfChars));
 		threadGuard.unlock();
 	}
-
 }
