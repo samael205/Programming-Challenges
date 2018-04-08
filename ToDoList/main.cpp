c83427b10dd7e4d7184eeb3fb464046876b783a2
Blooser <Blooser@protonmail.com>
2018-04-08 17:54:19 +0200

    set app icon

--- ToDoList/main.cpp
+++ ToDoList/main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char ** argv){
     QApplication app(argc, argv);
+    QIcon icon(QDir::currentPath() + "/icons/todo.png");
+    app.setWindowIcon(icon);
+
     MainWindow productivity;
     productivity.show();
+
     return app.exec();
 }
