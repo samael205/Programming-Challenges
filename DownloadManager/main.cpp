7c58b919acdf26c714e4097748e916420ecaced4
Blooser <Blooser@protonmail.com>
2018-06-20 19:40:16 +0200

    Set window icon

--- DownloadManager/main.cpp
+++ DownloadManager/main.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char ** argv){
     QApplication app(argc, argv);
+    Q_INIT_RESOURCE(icons);
+    app.setWindowIcon(QIcon(":/resources/icons/download.png"));
 
     MainWindow downloadManager;
     downloadManager.show();
