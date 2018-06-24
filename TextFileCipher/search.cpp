e2136271f4bb006483ac1e3342f04965c88c1921
Blooser <Blooser@protonmail.com>
2018-06-24 12:47:53 +0200

    Move search logic into one cpp file

---
+++ TextFileCipher/search.cpp
@@ -0,0 +1,23 @@
+#include <boost/filesystem.hpp>
+#include <vector>
+#include <string>
+
+namespace fs = ::boost::filesystem;
+
+void search(std::vector<std::string> & files, const fs::path &dirRoot){
+	if(!fs::exists(dirRoot) || !fs::is_directory(dirRoot))
+		return;
+	fs::recursive_directory_iterator it(dirRoot);
+	fs::recursive_directory_iterator end;
+	while(it != end){
+		if(fs::is_regular_file(*it) && it->path().extension() == ".txt")
+			files.push_back(it->path().string());
+		it++;
+	}
+}
+
+std::vector<std::string> getTextFiles(const std::string & path){
+	std::vector<std::string> files;
+	search(files, path);
+	return files;
+}
